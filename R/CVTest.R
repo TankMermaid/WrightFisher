6526f805f296963a7fec14691eb1026cde25c23d
reptalex <bigalculus@gmail.com>
2016-04-25 10:23:26 -0600

    Fixed bug for regress='time'

--- R/CVTest.R
+++ R/CVTest.R
@@ -4,8 +4,9 @@
 #' @param ntests Positive integer - number of randomly selected constant-volatility transformations (CVTs) for heteroskedasticity testing
 #' @param R Compositional matrix - columns must be species and rows timepoints.
 #' @param regress Either "f" or "t" - whether regrssion should be performed on the state of the CVT or on the time.
-#' @param formula Formula for regression of the jumps in CVTs vs time or state of cvts. Should be in form "DF ~ f" or "DF ~ tm + I(tm^2)" etc. Default is quadratic regression
+#' @param formula Formula for regression of the jumps in CVTs vs time or state of cvts. Should be in form "DF ~ f" or "DF ~ tm + I(tm^2)" or  etc. Default is quadratic regression
 #' @param varformla Input argument for \code{bptest}. Default is quadratic regression.
+#' @param tm Input vector of timpoints to be used if \code{regress}='time'
 #' @return vector of p-values from Bruesh-Pagan heteroskedasticity tests.
 #' @examples
 #' library(plotrix)
@@ -21,19 +22,19 @@
 #' par(mfrow=c(1,3))
 #' stackpoly(R,stack=T,main='Community Dynamics',xlab='time',ylab='Abundance')
 #' 
-#' #This will take ~10 seconds
-#' Pvalsf <- CVTest(1000,R)
-#' Pvalst <- CVTest(1000,R,regress='time')
+#' Pvalsf <- CVTest(100,R)
+#' Pvalst <- CVTest(100,R,regress='time')
 #' 
 #' plot(ecdf(Pvalsf),xlab='P',ylab='#',main='P-values from regress="f"')
 #' lines(c(0,1),c(0,1),lwd=4,col='blue')
 #' 
 #' plot(ecdf(Pvalst),xlab='P',ylab='#',main='P-values from regress="time"')
+#' lines(c(0,1),c(0,1),lwd=4,col='blue')
 #' 
 #' #Uncorrected KS test
 #' ks <- ks.test(Pvals,runif(length(Pvals)))
 #' ks$p.value
-CVTest <- function(ntests,R,regress='f',formula=NULL,varformula=NULL,...){
+CVTest <- function(ntests,R,regress='f',formula=NULL,varformula=NULL,tm=NULL,...){
   
   if (!(regress %in% c('f','time'))){stop('unknown inputt "regress" - must be either "f" or "time"')}
   if (is.null(formula)){
@@ -54,6 +55,15 @@ CVTest <- function(ntests,R,regress='f',formula=NULL,varformula=NULL,...){
   
   p <- rep(0,ntests)
   b <- vector('list',ntests)
+  
+  if (regress=='time'){
+    if (is.null(tm)){
+      tm <- 1:dim(R)[1]
+      DT <- diff(tm)
+      tm <- tm[1:(length(tm)-1)]
+    }
+  }
+  
   for (nn in 1:ntests){
     
     cvt <- drawCVT(R)
@@ -61,14 +71,18 @@ CVTest <- function(ntests,R,regress='f',formula=NULL,varformula=NULL,...){
     DF <- cvt$DF
 
     if (regress=='f'){
+      
       dataset=model.frame(formula,environment())
       model <- lm(formula,dataset)
       # p[nn] <- bptest(model,varformula)$p.value
       p[nn] <- bptest(model,varformula,...)$p.value
       DF <- model$residuals^2
       b[[nn]] <- glm(varformula)$coefficients
+      
     } else {
-      tm <- 1:(dim(R)[1]-1)
+      
+      DF <- DF/sqrt(DT)
+      
       dataset=model.frame(formula,environment())
       model <- lm(formula,dataset)
       # p[nn] <- bptest(model,varformula)$p.value
